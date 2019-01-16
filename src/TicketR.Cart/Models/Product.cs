3ec7599d6593a6bdaf318eb8401efe66581ef068
Wojciech Kłodkowski PGS <wklodkowski@pgs-soft.com>
2019-01-16 13:36:49 +0100

    Infrastructure-message-queue: Remove test projects

--- src/TicketR.Cart/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicketR.Cart.Models
-{
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
--- src/TicketR.Cart/Services/Interfaces/ICartService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TicketR.Cart.Services.Interfaces
-{
-    public interface ICartService
-    {
-        void ChangePrice(int productId, decimal newPrice, decimal oldPrice);
-    }
-}
